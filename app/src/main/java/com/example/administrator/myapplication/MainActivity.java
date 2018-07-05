8fabafddb5c3cf103a8c6e8df7713a6c176e9748
siguangzhou1 <39480852+siguangzhou1@users.noreply.github.com>
2018-07-05 16:22:04 +0800

    github更新
    在线第一次更新

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "提示", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "提示123", Toast.LENGTH_SHORT).show();
     }
 }
